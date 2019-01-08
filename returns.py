542959c063fa10317a3e92fb7b3ca4261a559f46
Tabrez Iqbal <tabrez.iqbal@gmail.com>
2019-01-08 20:02:44 +0530

    extract some functions and write tests for them

--- returns.py
+++ returns.py
@@ -35,50 +35,25 @@ def to_currency_fmt(num):
     return list(map(num_to_currency, num))
 
 def future_values(monthly, periods, rate=0.10, init=0, r=2):
-  final_amount = np.round(np.fv(rate/12,
+  return np.round(np.fv(rate/12,
                                 periods,
                                 -1 * monthly,
                                 init,
                                 when='begin'), r)
-  return to_currency_fmt(final_amount)
+  #return to_currency_fmt(final_amount)
 
-def try_future_values(monthly=1000):
-    periods = make_periods(5, 41, 5)
-    values = future_values(monthly, periods)
-    return make_df(periods/12, values, ['Years', 'Final Amount'])
+def invested_amount(monthly=1000, start=5, end=41, step=10, r=2):
+    amt_per_step = [monthly * 12 * step]
+    return (np.round(np.cumsum(
+        amt_per_step * len(range(start, end, step))), r))
 
-def future_values_diff_rates(monthly,
-                             rates,
-                             start=10,
-                             end=21,
-                             step=10,
-                             init=0,
-                             r=2):
-    periods = np.array([iter * 12 for iter in range(start, end, step)])
-    invested_amount = (np.round(np.cumsum([monthly * 12 * step]
-                                          * len(range(start, end, step))), r))
-    invested_amount_str = list(map(num_to_currency, invested_amount))
-
+def future_values_diff_rates(monthly, rates, periods, init=0, r=2):
     rates = [r/100 for r in rates]
-    res = []
-    for rate in rates:
-        final_amount = np.round(np.fv(rate/12,
-                                      periods,
-                                      -1 * monthly,
-                                      init,
-                                      when='begin'), r)
-        final_amount_str = list(map(num_to_currency, final_amount))
-        res.append(final_amount_str)
-    result = np.concatenate(([periods/12],
-                             [invested_amount_str],
-                             res), axis=0)
-    return result.transpose()
-
-def try_future_values_diff_rates(monthly=1000):
-    rates=[-8,3.5,4.62,5.6,7,7.5,10]
-    data = future_values_diff_rates(monthly, rates)
-    headers = ['Years', 'Invested Amount'] + list(map(str, rates))
-    return pd.DataFrame(data, columns=headers)
+    return [np.round(np.fv(rate/12,
+                           periods,
+                           -1 * monthly,
+                           init,
+                           when='begin'), r) for rate in rates]
 
 def future_values_df(monthly=1000, rate=0.10, start=5, end=41, step=5, init=0, r=2):
   periods = np.array([iter * 12 for iter in range(start, end, step)])
@@ -113,27 +88,47 @@ def try_future_values_df(monthly=1000):
                'Times Increased']
     return pd.DataFrame(data, columns=headers)
 
-def inflation_adjusted(init, rate=-0.08, start=7, end=43, step=7, r=2):
-    periods = np.array([iter for iter in range(start, end, step)])
-    print(periods)
-    after_inflation = np.round(np.fv(rate,
-                                   periods,
-                                   0,
-                                   -1 * init,
-                                   when='begin'), r)
-    after_inflation_str = list(map(num_to_currency, after_inflation))
-    return np.stack((periods, after_inflation_str), axis=-1)
-
-def try_inflation_adjusted(init=50000):
-    data = inflation_adjusted(init)
-    headers = ['Years',
-               'Future Value']
-    return pd.DataFrame(data, columns=headers)
+def inflation_adjusted(init, periods, rate=-0.08, r=2):
+    return np.round(np.fv(rate/12,
+                          periods,
+                          0,
+                          -1 * init,
+                          when='begin'), r)
 
 def main():
-  return try_future_values(1000)
+  # return try_future_values(1000)
   # return try_future_values_df(1000)
-  # return try_inflation_adjusted(100000)
+  return try_inflation_adjusted(100000)
   # return try_future_values_diff_rates(20000)
+  #  return None
+
+def try_future_values(monthly=1000):
+    periods = make_periods(5, 41, 5)
+    values = to_currency_fmt(future_values(monthly, periods))
+    return make_df(periods/12, values, ['Years', 'Final Amount'])
+
+def try_inflation_adjusted(init=50000):
+    #periods = np.array([iter for iter in range(start, end, step)])
+    periods = make_periods(start=7, end=43, step=7)
+    after_inflation = inflation_adjusted(init, periods)
+    values = to_currency_fmt(after_inflation)
+    headers = ['Years',
+               'Future Value']
+    data = make_df(periods, values, headers)
+    return pd.DataFrame(data, columns=headers)
+
+def try_future_values_diff_rates(monthly=1000):
+    rates=[-8,3.5,4.62,5.6,7,7.5,10]
+    (start, end, step) = (10, 31, 10)
+    periods = make_periods(start, end, step)
+    invested_amount_str = to_currency_fmt(invested_amount(monthly, start, end, step))
+    future_values_dr = future_values_diff_rates(monthly, rates, periods)
+    res = [to_currency_fmt(arr) for arr in future_values_dr]
+    result = np.concatenate(([periods/12],
+                             [invested_amount_str],
+                             res), axis=0)
+    data = result.transpose()
+    headers = ['Years', 'Invested Amount'] + list(map(str, rates))
+    return pd.DataFrame(data, columns=headers)
 
 main()
--- test_returns.py
+++ test_returns.py
@@ -24,3 +24,37 @@ def test_to_currency_fmt_values():
     assert re.match(r'₹[ ]?123.00', to_currency_fmt(nums)[0])
     assert re.match(r'₹[ ]?1.25K', to_currency_fmt(nums)[1])
     assert re.match(r'₹[ ]?7.65L', to_currency_fmt(nums)[2])
+
+def test_future_values():
+    periods = make_periods(start=5, end=41, step=5)
+    values = future_values(1000, periods)
+    expected = [78082.38, 206552.02, 417924.27, 765696.91, 1337890.35,
+                2279325.32, 3828276.7 , 6376780.24]
+    assert np.all(np.isclose(values, expected))
+
+def test_invested_amount():
+    values = invested_amount(1000)
+    assert np.allclose(values, [120000, 240000, 360000, 480000])
+
+def test_inflation_adjusted():
+    periods = make_periods(start=7, end=43, step=7)
+    values = inflation_adjusted(100000, periods)
+    expected = [57013.9 , 32505.85, 18532.86, 10566.31,  6024.26,  3434.67]
+    assert np.allclose(values, expected)
+
+def test_future_values_diff_rates():
+    rates=[-8,3.5,4.62,5.6,7,7.5,10]
+    periods = make_periods(start=10, end=31, step=10)
+    values = future_values_diff_rates(1000, rates, periods)
+    expected = [np.array([82229.08, 119078.15, 135591.21]),
+                np.array([143850.86, 347880.97, 637266.03]),
+                np.array([152751.53, 394990.71, 779142.77]),
+                np.array([161118.44, 442816.87, 935336.58]),
+                np.array([174094.47,  523965.4 , 1227087.49]),
+                np.array([179042.41,  557191.54, 1355866.96]),
+                np.array([206552.02,  765696.91, 2279325.32])]
+    assert np.allclose(np.array(values).flatten(),
+                       np.array(expected).flatten())
+
+def test_fail():
+    assert True
